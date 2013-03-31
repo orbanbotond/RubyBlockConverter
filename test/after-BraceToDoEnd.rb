6bb17d9142857701e6384abff99a9319b7c7fa24
Orban Botond <orbanbotond@gmail.com>
2013-03-31 18:00:23 +0300

    Adding the default commands file. This way the command will be shown in the Command Palette. The commands are changed and refactored to a toggle command. The same key changes the do-end to brackets and vice versa. FIX #1.

---
+++ test/after-BraceToDoEnd.rb
@@ -0,0 +1,94 @@
+  f do
+  end
+  f do |a|
+  end
+  f do |a|
+  end
+  f do
+  end
+  f do
+    a
+    b
+  end
+  f do
+    a
+  end
+  f do
+    a
+  end
+  f do
+    a
+    f do |a|
+      a
+    end
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do
+  end
+  f do |a|
+  end
+  f do |a|
+  end
+  f do
+  end
+  f do
+    a
+  end
+  f do
+    a
+  end
+  f do
+    a
+  end
+  f do
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do |a|
+    a
+  end
+  f do
+    g{
+    }
+    cursor here
+  end
